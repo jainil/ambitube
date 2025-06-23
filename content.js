f6b55bd92456d34171238a25efc63a8416eb6ebf
jainil <jainil@users.noreply.github.com>
2025-06-23 22:08:34 +0530

    fix most css for max ambient mode

--- content.js
+++ content.js
@@ -2,6 +2,7 @@ if (window.top === window) {
   chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.action === 'toggleCSS') {
       document.body.classList.toggle('ambitube-enabled');
+      window.dispatchEvent(new Event('resize'));
     }
   });
 }
